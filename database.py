0f927d87f761f451f961631a70e00a88d2f0c9d9
Vlad <VladLVOWork@yandex.ru>
2017-12-28 17:00:47 +0300

    убрал все ошибки

---
+++ database.py
@@ -0,0 +1,23 @@
+# pyl int: disa ble=C0103
+
+"""documentation modul"""
+
+#import fdb
+
+
+class DataBase:
+    """documentation class"""
+
+    def __init__(self):
+        """documentation function"""
+        self.value = 0
+
+    def test_func(self):
+        """documentation function"""
+        self.value = 55
+        print("sdsdsdsdsd")
+
+    def test_func2(self):
+        """documentation function"""
+        self.value = 55
+        print("sdsdsdsdsd")
